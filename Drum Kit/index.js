8249dcc5ad8f3888e887eecc1045341d99121101
karankashyap04 <67017734+karankashyap04@users.noreply.github.com>
2022-07-28 12:16:35 +0530

    Added Drum Kit project

---
+++ Drum Kit/index.js
@@ -0,0 +1,68 @@
+// document.querySelector("button").addEventListener("click", handleClick);
+
+// function handleClick() {
+//     alert("I got clicked!");
+// }
+
+
+function playSound(key) { //plays the sound based on the key pressed
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        default:
+            console.log(buttonInnerHTML);
+    }
+}
+
+function buttonAnimation(key) {
+    var activeButton = document.querySelector("." + key);
+    activeButton.classList.add("pressed");
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100); // the time is in milliseconds, so this is 100ms, i.e. 0.1s
+}
+
+
+// Detecting Button Press
+var buttons = document.querySelectorAll(".drum");
+for (var i=0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function () {
+        
+        var buttonInnerHTML = this.innerHTML;
+        playSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
+    }) //passing in an anonymous function as the second parameter
+}
+
+// Detecting Keyboard Press
+document.addEventListener("keydown", function(event) {
+    playSound(event.key);
+    buttonAnimation(event.key);
+});
