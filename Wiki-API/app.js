52af6d65f40d2c30e7c4255c064e093d2dbe112a
karankashyap04 <67017734+karankashyap04@users.noreply.github.com>
2022-08-05 15:16:26 +0530

    Added Wiki-API project (Made my own RESTful API)

---
+++ Wiki-API/app.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/wikiDB");
+
+const articleSchema = mongoose.Schema({
+    title: String,
+    content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
+
+
+// Requests targetting all articles:
+
+app.route("/articles")
+.get(function(req, res) {
+    Article.find({}, function(err, foundArticles) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(foundArticles);
+        }
+    });
+})
+
+.post(function(req, res) {
+    const newArticle = new Article({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newArticle.save(function(err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("Successfully added a new article!");
+        }
+    });
+})
+
+.delete(function(req, res) {
+    // Not specifying delete condition so we delete everything:
+    Article.deleteMany(function(err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("Successfully deleted all articles");
+        }
+    });
+});
+
+
+// Requests targetting a specific article
+
+app.route("/articles/:articleTitle")
+.get(function(req, res) {
+    Article.findOne({title: req.params.articleTitle}, function(err, foundArticle) {
+        if (err) {
+            res.send(err);
+        } else if (foundArticle) {
+            res.send(foundArticle);
+        } else {
+            res.send("No article with that title was found.");
+        }
+    });
+})
+
+.put(function(req, res) {
+    Article.replaceOne(
+        {title: req.params.articleTitle}, // condition
+        {title: req.body.title, content: req.body.content}, // update
+        // {overwrite: true},
+        function(err) {
+            if (err) {
+                console.log(err);
+                res.send(err);
+            } else {
+                res.send("Successfully updated the selected article.");
+            }
+        }
+    );
+})
+
+.patch(function(req, res) {
+    Article.updateOne(
+        {title: req.params.articleTitle},
+        {$set: req.body}, // the req body is basically a JS object which will land up being
+        // in the same format as how the update is to be specified
+        function(err) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.send("Successfully updated the selected article.")
+            }
+        }
+    );
+})
+
+.delete(function(req, res) {
+    Article.deleteOne({title: req.params.articleTitle}, function(err) {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send("Successfully deleted the selected article.");
+        }
+    });
+});
+
+
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
